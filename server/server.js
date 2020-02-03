b1387dbd939075aeeb5ff29a6552f025fc98ab99
Matthew Huynh <matthuynh@users.noreply.github.com>
2020-02-03 02:38:13 -0500

    COL-3 Add template code for the login route

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use("/api/users/register", require("./routes/auth/register"));
+app.use("/api/users/login", require("./routes/auth/login"))
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
