c883ec61eb236103e67235e942f43a667bbc33ae
Hans Patrick Paras <hanspatrick.paras@mail.utoronto.ca>
2020-02-17 13:43:56 -0500

    COL-34ImproveRegister: Finished most of the documentation of register

--- server/database.js
+++ server/database.js
@@ -1,13 +1,14 @@
 require('dotenv').config({path: './config/.env'});
 const { Sequelize, DataTypes, Model } = require('sequelize');
-
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     dialect: 'postgres',
     port: process.env.DB_PORT,
     logging: (process.env.DB_LOGGING == "TRUE" ? true : false)
 });
-
+console.log(process.env.DB_NAME);
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASS);
 try {
     db.authenticate();
     console.log('Connection has been established successfully.');
