9a996ead9bc0c38a35a39d4f0ddecf41296c6810
Matthew Huynh <matthuynh@users.noreply.github.com>
2020-02-20 18:45:24 -0500

    COL-7 :sparkles: Finish up base work on the back-end route

--- server/routes/github/getRepos.js
+++ server/routes/github/getRepos.js
@@ -5,12 +5,13 @@ const { check, validationResult } = require("express-validator");
 const db = require("../../database.js");
 
 // @route   GET /api/github/repos
-// @desc    Allows a user to create a new project
+// @desc    Return a list of GitHub repos that belongs to this user
 // @access  Public
 router.get(
 	"/",
 	[	
-		check("userid", "User ID is required").not().isEmpty()
+		check("username", "Username is required").not().isEmpty(),
+		check("visibility", "Visiblity of repo is required and must be one of 'public', 'private', or 'all'").isIn(['public', 'private', 'all'])
 	],
 	async (req, res) => {
 		try {
@@ -20,32 +21,53 @@ router.get(
 				return res.status(422).json({ errors: errors.array() });
 			}
 
-			// TODO: Get the GitHub OAuth token associated with this userID
-			let token = "token 5951a38d4b5528f2b74a412a4653575fa55375b6";
+			// Get the GitHub auth token for the given username
+			const user = await db.models.user.findOne({
+				where: {
+					username: req.body.username,
+				}
+			});
+			if (user === null) {
+				return res.status(404).json({ errorMessage: "That username was not found in the database" });
+			} 
+			let authToken = "token " + user.authToken;
+			// let authToken = " token 1689077dba9bea9325fbccf719c65bb34c984b0e"; // NOTE: Use this for debugging
 
-            // TODO: Get the repos associated with this user
-            let response = await axios.get("https://api.github.com/user/repos", {
+            // Get the repos associated with this user
+            let githubResponse = await axios.get("https://api.github.com/user/repos", {
 				headers:
 					{
 						"Accept": "application/vnd.github.v3+json",
-						"Authorization": token
+						"Authorization": authToken
 					},
 				params:
 					{
-						"visibility": "public",
-						"affiliation": "owner"
+						"visibility": req.body.visibility,
+						"affiliation": "owner",
+						"sort": "created",
+						"direction": "desc"
 					}
-			
 			});
-			console.log(response);
-
-
-			res.status(200).json({ 
-				result: "Success",
-			});
+			if (githubResponse.status == 200) {
+				// console.log(githubResponse);
+				let repos = githubResponse.data;
+				let repoData = {};
+				for (let i = 0; i < repos.length; i++) {
+					// TODO: Consult with the team to figure out what other fields we want to save
+					repoData[repos[i].name] = {};
+					repoData[repos[i].name]["url"] = repos[i].html_url;
+					repoData[repos[i].name]["description"] = repos[i].description;
+					repoData[repos[i].name]["creation_date"] = repos[i].created_at;
+				}
+				// console.log(repoData);
+				return res.status(200).json(repoData);
+			}
+			res.status(500).json({ errorMessage: "Internal server error" });
 		} catch (err) {
-            console.error(err);
-            res.status(500).json({ errorMessage: "Internal server error" });
+			if (err.response && err.response.status == 401) {
+				return res.status(401).json({ errorMessage: "GitHub auth token associated with the username is invalid" });
+			}
+            return res.status(500).json({ errorMessage: "Internal server error" });
         }
 	}
 );
