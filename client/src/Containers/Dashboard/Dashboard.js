f14fcd9d8d8fcf649862e2c9642dd958e8746dc1
FurqanXPS <36116798+Furqan17@users.noreply.github.com>
2020-02-25 22:56:33 -0500

    COL-4 :construction: Intermediate Static Data Push

--- client/src/Containers/Dashboard/Dashboard.js
+++ client/src/Containers/Dashboard/Dashboard.js
@@ -1,19 +1,51 @@
 import React from "react";
 import { NavigationBar } from "../../components/base/NavigationBar";
-import { Container, Row, Col, Jumbotron } from "react-bootstrap";
+import { 
+    Alert, 
+    Card, 
+    Button, 
+    Container, 
+    Row, 
+    Col, 
+    Jumbotron 
+} from "react-bootstrap";
 
 // Style Import
 import "../../css/Dashboard.css";
 
 const Dashboard = () => (
     <div>
-        {/* <NavigationBar /> */}
-        <Row>
+        <NavigationBar />
+            <div style={{margin: "50px"}}>
+                <h1>&#10024;Welcome to your dashboard temporaryuser&#10024;</h1>
+            </div>
+        <Row style={{ margin: "25px"}}>
+            
             {/* Col 1 */}
-            <Col md={2} sm={2} lg={2} xl={2}>
+            <Col md={3} sm={3} lg={3} xl={3}>
                 <Container>
                     <div>
-                        1 of 3
+                    <h4>Nearby Hackathons</h4>
+                        <Card>
+                            <Card.Body>
+                                {/* Card Index 0 */}
+                                <Card.Title>&#9989; <a href="/">TheRBajaj</a> joined your <a href="/">Optimize.me</a> project!</Card.Title>
+                                <Card.Text>
+                                    &#128172; <a href="/">TheRBajaj</a> wrote: I think this project will go well!
+                                </Card.Text>
+                                <Button variant="success">Check out Project</Button>
+                            </Card.Body>
+
+                            <Card.Body>
+                                {/* Card Index 0 */}
+                                <Card.Title>&#10067; <a href="/">matthuyhn</a> is requesting to join your <a href="/">Stock Trading</a> project!</Card.Title>
+                                <Card.Text>
+                                    &#128172; <a href="/">matthuyhn</a> wrote: I love finance and would like to join this project with you!
+                                </Card.Text>
+                                <Button variant="success">View Project</Button>
+                            </Card.Body>
+
+                        </Card>
                     </div>
                 </Container>
             </Col>
@@ -22,18 +54,69 @@ const Dashboard = () => (
             <Col >
                 <Container>
                     <div>
-                        <Jumbotron>
-                            <h3>Welcome to your dashboard $insertnamehere</h3>
-                        </Jumbotron>
-                        2 of 3
+                        <Alert variant={'primary'}>
+                            here are your notification based on your follow preferences
+                        </Alert>
+
+                        <h4>Recent Activities</h4>
+                        <Card>
+                            <Card.Body>
+                                {/* Card Index 0 */}
+                                <Card.Title>&#9989; <a href="/">TheRBajaj</a> joined your <a href="/">Optimize.me</a> project!</Card.Title>
+                                <Card.Text>
+                                    &#128172; <a href="/">TheRBajaj</a> wrote: I think this project will go well!
+                                </Card.Text>
+                                <Button variant="success">Check out Project</Button>
+                            </Card.Body>
+
+                            <Card.Body>
+                                {/* Card Index 0 */}
+                                <Card.Title>&#10067; <a href="/">matthuyhn</a> is requesting to join your <a href="/">Stock Trading</a> project!</Card.Title>
+                                <Card.Text>
+                                    &#128172; <a href="/">matthuyhn</a> wrote: I love finance and would like to join this project with you!
+                                </Card.Text>
+                                <Button variant="success">View Project</Button>
+                            </Card.Body>
+
+                            <Card.Body>
+                                {/* Card Index 0 */}
+                                <Card.Title>&#11088; <a href="/">Furqan17</a> starred your <a href="/">207 Paint</a> project!</Card.Title>
+                                <Card.Text>
+                                    &#128172; <a href="/">Furqan17</a> wrote: hey this looks familiar...
+                                </Card.Text>
+                                <Button variant="success">View Project Stars</Button>
+                            </Card.Body>
+                        </Card>
                     </div>
                 </Container>
             </Col>
 
             {/* Col 4 */}
-            <Col md={2} sm={2} lg={2} xl={2}>
+            <Col md={3} sm={3} lg={3} xl={3}>
                 <Container>
-                    <div>3 of 3</div>
+                    <div>
+                        <h4>Nearby Hackathons</h4>
+                        <Card>
+                            <Card.Body>
+                                {/* Card Index 0 */}
+                                <Card.Title>&#9989; <a href="/">TheRBajaj</a> joined your <a href="/">Optimize.me</a> project!</Card.Title>
+                                <Card.Text>
+                                    &#128172; <a href="/">TheRBajaj</a> wrote: I think this project will go well!
+                                </Card.Text>
+                                <Button variant="success">Check out Project</Button>
+                            </Card.Body>
+
+                            <Card.Body>
+                                {/* Card Index 0 */}
+                                <Card.Title>&#10067; <a href="/">matthuyhn</a> is requesting to join your <a href="/">Stock Trading</a> project!</Card.Title>
+                                <Card.Text>
+                                    &#128172; <a href="/">matthuyhn</a> wrote: I love finance and would like to join this project with you!
+                                </Card.Text>
+                                <Button variant="success">View Project</Button>
+                            </Card.Body>
+
+                        </Card>
+                    </div>
                 </Container>
             </Col>
         </Row>
