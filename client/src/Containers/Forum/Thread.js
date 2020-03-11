898d2c1f97787748581994d015646af3ab6e5e15
Jarrod Servilla <jarrod.servilla@gmail.com>
2020-03-11 16:52:39 -0400

    COL-16 :hammer: Added useState and useEffect to Thread and Post, in preparation for future

--- client/src/Containers/Forum/Thread.js
+++ client/src/Containers/Forum/Thread.js
@@ -1,27 +1,60 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Container, Breadcrumb } from "react-bootstrap";
+import { Container, Breadcrumb, Form, Button, Card } from "react-bootstrap";
 import { NavigationBar } from "../../components/base/NavigationBar";
 import Post from '../../components/specialized/Forum/Post';
 
 const Thread = (props) => {
+
+  //const [threadId, setThreadId] = useState("");
+  const [title, setTitle] = useState("");
+  const [parentPath, setParentPath] = useState("");
+  const [subforum, setSubforum] = useState("");
+  const [submitter, setSubmitter] = useState("");
+  const [createdAt, setCreatedAt] = useState("");
+  const [posts, setPosts] = useState([]);
+  
+  useEffect(
+    () => {
+      setTitle(props.title);
+      setParentPath(props.parentPath);
+      setSubforum(props.subforum);
+      setSubmitter(props.submitter);
+      setCreatedAt(props.createdAt);
+      setPosts(props.posts);
+    }, [props]);
+
   return (
     <div>
     <NavigationBar />
     <Container>
     <Breadcrumb>
       <Breadcrumb.Item><Link to="/forum/">Home</Link></Breadcrumb.Item>
-      <Breadcrumb.Item><Link to={props.parentPath}>{props.subforum}</Link></Breadcrumb.Item>
-      <Breadcrumb.Item active>{props.title}</Breadcrumb.Item>
+      <Breadcrumb.Item><Link to={parentPath}>{subforum}</Link></Breadcrumb.Item>
+      <Breadcrumb.Item active>{title}</Breadcrumb.Item>
     </Breadcrumb>
     <div className="d-flex flex-column">
-    <h3 className="text-left">{props.title}</h3>
-    <h6 className="text-left">{"Posted by: " + props.submitter + " on " + props.createdAt}</h6>
+    <h3 className="text-left">{title}</h3>
+    <h6 className="text-left">{"Posted by: " + submitter + " on " + createdAt}</h6>
     </div>
-    {props.posts.map((reply) => 
+    {posts.map((reply) => 
       <Post key={reply.id} submitter={reply.submitter} status={reply.status}
       createdAt={reply.createdAt} content={reply.content}/>
     )}
+    <div className="p-2">
+      <Card className="border-0">
+        <Form>
+          <Form.Group className="text-left" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Submit a Reply</Form.Label>
+            <Form.Control as="textarea" rows="3" />
+          </Form.Group>
+          <div className="align-items-start">
+            <Button variant="primary" type="submit">Submit</Button>
+          </div>
+          
+        </Form>
+      </Card>
+    </div>
     </Container>
     
     </div>
--- client/src/components/specialized/Forum/Post.js
+++ client/src/components/specialized/Forum/Post.js
@@ -1,38 +1,52 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 import { Card, Container, Row, Col } from 'react-bootstrap';
 
 import "../../../css/Forum.css";
 
 
-const Post = (props) => (
-  <div className="p-2">
-     <Card fluid hoverable="true">
-      <Card.Body className="poster">
-        <Container>
-          <Row>
-            <Col xs={2} className="post">
-              <p className="submitter">{props.submitter}</p>
-              <p>{props.status}</p>
-            </Col>
-            <Col>
-            <Card.Text className="post text-left">
-              <p className="small">{props.createdAt}</p>
-              <hr
-                style={{
-                    color: "grey",
-                    backgroundColor: "grey",
-                    height: 1
-                }}
-             />
-              <p>{props.content}</p>
-            </Card.Text>
-            </Col>
-          </Row>
-        </Container>
-      </Card.Body>
-    </Card>
-  </div>
-);
+const Post = (props) => {
+
+  const [content, setContent] = useState("");
+  const [submitter, setSubmitter] = useState("");
+  const [createdAt, setCreatedAt] = useState("");
+  
+  useEffect(
+    () => {
+      setContent(props.content);
+      setSubmitter(props.submitter);
+      setCreatedAt(props.createdAt);
+    }, [props]);
+
+  return (
+    <div className="p-2">
+      <Card fluid hoverable="true">
+        <Card.Body className="poster">
+          <Container>
+            <Row>
+              <Col xs={2} className="post">
+                <p className="submitter">{submitter}</p>
+                <p>{props.status}</p>
+              </Col>
+              <Col>
+              <Card.Text className="post text-left">
+                <p className="small">{createdAt}</p>
+                <hr
+                  style={{
+                      color: "grey",
+                      backgroundColor: "grey",
+                      height: 1
+                  }}
+              />
+                <p>{content}</p>
+              </Card.Text>
+              </Col>
+            </Row>
+          </Container>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
 
 export default Post;
