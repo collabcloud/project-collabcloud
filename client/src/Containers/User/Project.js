cd36de9abaf8700f1d2d99a2fac54a99e4db8f60
Matthew Huynh <matthuynh@users.noreply.github.com>
2020-03-12 05:23:02 -0400

    COL-12 :pencil: Remove some extra console logs

--- client/src/Containers/User/Project.js
+++ client/src/Containers/User/Project.js
@@ -19,7 +19,7 @@ import PropTypes from "prop-types";
 
 // TODO: Get the PID of this project from the store, or the name of the project from the dynamic path
 // This is the PID of the project whose information we want to get
-const projectId = "a720142a-4076-5c0d-9edc-5236c9be2b55";
+const projectId = "3c9e9769-3e21-5bb7-b24f-fed83f7ff06b";
 
 const Project = ({ getProjectInformation, updateProject, deleteProject, projectInformation, updateSuccess, deleteSuccess }) => {
     const history = useHistory();
--- client/src/Reducers/loginReducer.js
+++ client/src/Reducers/loginReducer.js
@@ -8,16 +8,13 @@ const initialState = {
 };
 
 export default (state = initialState, action) => {
-    // console.log("action type "+action.type);
     switch (action.type) {
         case LOGIN:
-            console.log("loginReducer.js line 14: loggedIn value is true");
             return {
                 ...state,
                 loggedIn: true
             };
         default:
-            // console.log("loginReducer.js line 20");
             return state;
     }
 };
