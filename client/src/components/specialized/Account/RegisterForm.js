9043ed184862aabb1a7af5df317dbff1dfe237a0
Matthew Huynh <matthuynh@users.noreply.github.com>
2020-05-09 18:58:49 -0400

    COL-1073 Improve styling of Register2 page. Add tooltips

--- client/src/components/specialized/Account/RegisterForm.js
+++ client/src/components/specialized/Account/RegisterForm.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+// import Form from "react-bootstrap/Form";
+// import Button from "react-bootstrap/Button";
+import { Form, Tooltip, OverlayTrigger, Button } from "react-bootstrap";
 import ReactTags from "react-tag-autocomplete";
 
 import "../../../css/RegisterForm.css";
@@ -113,79 +114,91 @@ const RegisterForm = withRouter(
     return (
       <div>
         <Form onSubmit={onSubmit} className="register-form">
-          <Form.Group controlId="formBasicUsername">
-            <Form.Label className="float-left">Username</Form.Label>
-            <Form.Control
-              required
-              name="username"
-              type="text"
-              placeholder="Username"
-              value={formData.username}
-              onChange={onChange}
-            />
-            <Form.Control.Feedback type="invalid">
-              Please enter a username
-            </Form.Control.Feedback>
-          </Form.Group>
+          <OverlayTrigger placement="right" overlay={<Tooltip id="tooltip-disabled">This needs to be the same as your GitHub username</Tooltip>}>
+            <Form.Group controlId="formBasicUsername">
+              <Form.Label className="float-left">Username</Form.Label>
+              <Form.Control
+                required
+                name="username"
+                type="text"
+                placeholder="Username"
+                value={formData.username}
+                onChange={onChange}
+                />
+              <Form.Control.Feedback type="invalid">
+                Please enter a username
+              </Form.Control.Feedback>
+            </Form.Group>
+          </OverlayTrigger>
 
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label className="float-left">Password</Form.Label>
-            <Form.Control
-              required
-              name="password"
-              type="password"
-              placeholder="Password"
-              value={formData.password}
-              onChange={onChange}
-            />
-            <Form.Control.Feedback type="invalid">
-              Please enter your password
-            </Form.Control.Feedback>
-          </Form.Group>
+          <OverlayTrigger placement="right" overlay={<Tooltip id="tooltip-disabled">Your password must be at least 8 characters long. All passwords are hashed. Your password hashes will never be shared with anyone.</Tooltip>}>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label className="float-left">Password</Form.Label>
+              <Form.Control
+                required
+                name="password"
+                type="password"
+                placeholder="Password"
+                value={formData.password}
+                onChange={onChange}
+              />
+              <Form.Control.Feedback type="invalid">
+                Please enter your password
+              </Form.Control.Feedback>
+            </Form.Group>
+          </OverlayTrigger>
 
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label className="float-left">Confirm Password</Form.Label>
-            <Form.Control
-              required
-              name="confirmpassword"
-              type="password"
-              placeholder="Confirm Password"
-              value={formData.confirmpassword}
-              onChange={onChange}
-            />
-            <Form.Control.Feedback type="invalid">
-              Please enter password confirmation
-            </Form.Control.Feedback>
-          </Form.Group>
+          <OverlayTrigger placement="right" overlay={<Tooltip id="tooltip-disabled">Enter the same password as above</Tooltip>}>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label className="float-left">Confirm Password</Form.Label>
+              <Form.Control
+                required
+                name="confirmpassword"
+                type="password"
+                placeholder="Confirm Password"
+                value={formData.confirmpassword}
+                onChange={onChange}
+              />
+              <Form.Control.Feedback type="invalid">
+                Please enter password confirmation
+              </Form.Control.Feedback>
+            </Form.Group>
+          </OverlayTrigger>
 
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label className="float-left">Email</Form.Label>
-            <Form.Control
-              required
-              name="email"
-              type="email"
-              placeholder="Email"
-              value={formData.email}
-              onChange={onChange}
-            />
-            <Form.Control.Feedback type="invalid">
-              Please enter Email{" "}
-            </Form.Control.Feedback>
-          </Form.Group>
-          <label>Interested Languages/Tech</label>
+          <OverlayTrigger placement="right" overlay={<Tooltip id="tooltip-disabled">Your email will be used to reset your password in case you forget. Your email will never be shared with anyone.</Tooltip>}>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label className="float-left">Email</Form.Label>
+              <Form.Control
+                required
+                name="email"
+                type="email"
+                placeholder="Email"
+                value={formData.email}
+                onChange={onChange}
+              />
+              <Form.Control.Feedback type="invalid">
+                Please enter Email{" "}
+              </Form.Control.Feedback>
+            </Form.Group>
+          </OverlayTrigger>
+
+          <p id="interested-languages-label">Interested Languages/Tech</p>
           <ReactTags
             className="item"
+            id="language-tag-input"
             tags={tech}
             suggestions={tech_suggestions}
             handleDelete={handleDelete}
             handleAddition={handleAddition}
           />
-          <Form.Group controlId="formBasicCheckbox">
+
+          <Form.Group controlId="formBasicCheckbox" id="tos-checkbox">
             <Form.Check type="checkbox" name="terms" className="float-left" />
             <label htmlFor="terms">
               I agree to the <a href="register2">Terms of service </a>
             </label>
           </Form.Group>
+        
           <ul
             style={{ listStyleType: "none", margin: "auto", padding: 0 }}
             className="errors"
@@ -194,17 +207,18 @@ const RegisterForm = withRouter(
               return <li key={index}>{value}</li>;
             })}
           </ul>
+
           <Button variant="outline-primary" type="submit" block>
-            Submit
+            Complete Registration
           </Button>
         </Form>
         <div>
           <img
-            alt=""
+            alt="CollabCloud-logo"
             src={require("../../../logo.png")}
             width="70"
             height="35"
-            style={{ marginTop: 40, marginBottom: 20 }}
+            style={{ marginTop: 20, marginBottom: 20 }}
           />
         </div>
       </div>
